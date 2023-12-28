374dcd1c570df8b981b1170f9cb94a29838162b3
Adam Hollett <mail@adamhollett.com>
2023-12-28 00:24:53 -0330

    Add reset button

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const WIDTH = 16;
+const DEFAULT_GRID_SIZE = 16;
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => handleReset())
 
 const gridElem = document.querySelector("#grid");
 
-function resetGrid(width = WIDTH) {
+function handleReset() {
+  let gridSize = prompt("Enter a grid size from 1 to 100");
+
+  if (gridSize < 1 || gridSize > 100) {
+    gridSize = prompt("Invalid size. Enter a grid size from 1 to 100");
+  }
+
+  return resetGrid(gridSize);
+}
+
+function resetGrid(width = DEFAULT_GRID_SIZE) {
+  gridElem.innerHTML = "";
+
   for (let i = 1; i <= width; i++) {
     addRow(i);
 
